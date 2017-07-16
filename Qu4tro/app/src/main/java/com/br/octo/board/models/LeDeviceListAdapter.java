ed2dce7b25a90d5fba775219c0e00ade0528ef02
Endy Silveira <endy.silveira@gmail.com>
2017-07-16 18:35:22 -0300

    Improvements: Move List Adapter for its own file

---
+++ Qu4tro/app/src/main/java/com/br/octo/board/models/LeDeviceListAdapter.java
@@ -0,0 +1,8 @@
+package com.br.octo.board.models;
+
+/**
+ * Created by endysilveira on 16/07/17.
+ */
+
+public class LeDeviceListAdapter {
+}
--- Qu4tro/app/src/main/java/com/br/octo/board/modules/DeviceListActivity.java
+++ Qu4tro/app/src/main/java/com/br/octo/board/modules/DeviceListActivity.java
@@ -8,25 +8,21 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.br.octo.board.R;
 import com.br.octo.board.api_services.BluetoothHelper;
+import com.br.octo.board.models.LeDeviceListAdapter;
 import com.br.octo.board.modules.base.BaseActivity;
 
 import java.util.ArrayList;
@@ -137,111 +133,33 @@ public class DeviceListActivity extends BaseActivity implements BluetoothHelper.
     }
 
     public void scanLeDevice(boolean toScan) {
-        scanButton.setEnabled(!toScan);
+        if (mBluetoothAdapter.isEnabled()) {
+            scanButton.setEnabled(!toScan);
 
-        if (toScan) {
-            mLeDeviceListAdapter.clear();
+            if (toScan) {
+                mLeDeviceListAdapter.clear();
 
-            setTitle(R.string.scanning);
+                setTitle(R.string.scanning);
 
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    scanLeDevice(false);
-                }
-            }, SCAN_PERIOD);
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        scanLeDevice(false);
+                    }
+                }, SCAN_PERIOD);
 
-            mBluetoothLEScanner.startScan(filters, settings, mScanCallback);
-        } else {
-            setTitle(R.string.select_device);
-
-            mBluetoothLEScanner.stopScan(mScanCallback);
-        }
-    }
-
-    // Adapter for holding devices found through scanning.
-    private class LeDeviceListAdapter extends ArrayAdapter {
-        private Context context;
-        private ArrayList<BluetoothDevice> mLeDevices;
-        private LayoutInflater mInflator;
-
-        LeDeviceListAdapter(Context context, ArrayList<BluetoothDevice> devices) {
-            super(context, R.layout.device_name, devices);
-
-            this.mLeDevices = devices;
-            this.context = context;
-        }
-
-        class LeDeviceViewHolder {
-            TextView deviceAddress;
-            TextView deviceName;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            LeDeviceViewHolder LeDeviceItem;
-            // General ListView optimization code.
-
-            if (mInflator == null) {
-                mInflator = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            }
-
-            if (view == null) {
-                view = mInflator.inflate(R.layout.device_name, null);
-
-                LeDeviceItem = new LeDeviceViewHolder();
-
-                LeDeviceItem.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-                LeDeviceItem.deviceName = (TextView) view.findViewById(R.id.device_name);
-
-                view.setTag(LeDeviceItem);
+                mBluetoothLEScanner.startScan(filters, settings, mScanCallback);
             } else {
-                LeDeviceItem = (LeDeviceViewHolder) view.getTag();
-            }
+                setTitle(R.string.select_device);
 
-            BluetoothDevice device = mLeDevices.get(i);
-
-            final String deviceName = device.getName();
-            if (deviceName != null && deviceName.length() > 0) {
-                LeDeviceItem.deviceName.setText(deviceName);
-            }
-
-            LeDeviceItem.deviceAddress.setText(device.getAddress());
-
-            return view;
-        }
-
-        void addDevice(BluetoothDevice device) {
-            if (!mLeDevices.contains(device)) {
-                mLeDevices.add(device);
+                mBluetoothLEScanner.stopScan(mScanCallback);
             }
         }
-
-        BluetoothDevice getDevice(int position) {
-            return mLeDevices.get(position);
-        }
-
-        public void clear() {
-            mLeDevices.clear();
-        }
-
-        @Override
-        public int getCount() {
-            return mLeDevices.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return mLeDevices.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-
     }
+
+    //endregion
+
+    //region BT Callback
 
     // Device scan callback.
     private ScanCallback mScanCallback =
@@ -273,10 +191,6 @@ public class DeviceListActivity extends BaseActivity implements BluetoothHelper.
                     Log.e("Scan Failed", "Error Code: " + errorCode);
                 }
             };
-
-    // endregion
-
-    //region BT Callback
 
     @Override
     public void onMessageReceived(String message) {
