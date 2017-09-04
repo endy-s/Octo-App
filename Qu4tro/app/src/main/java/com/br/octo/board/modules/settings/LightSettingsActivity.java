7308fa9d073eea441df4f57b3359ea428051500e
Endy Silveira <endy.silveira@gmail.com>
2017-09-04 01:30:00 -0300

    Improvements: - Instantiate other preferences globally - Add BluetoothCallback - finish activity when disconnected and update screen when receiving an update from the board - Sets the flag at BTHelper - Added logics to update screen for Low Power mode - Improved the logics at sharedPreferenceChanged
    -- Need to test the updating of screen of Low Power and now for the BCAP too and check the second option (without flags and counter and changing the changedListener)

--- Qu4tro/app/src/main/java/com/br/octo/board/modules/settings/LightSettingsActivity.java
+++ Qu4tro/app/src/main/java/com/br/octo/board/modules/settings/LightSettingsActivity.java
@@ -19,10 +19,14 @@ import com.br.octo.board.modules.base.AppCompatPreferenceActivity;
  * Created by Endy.
  */
 public class LightSettingsActivity extends AppCompatPreferenceActivity implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+        SharedPreferences.OnSharedPreferenceChangeListener, BluetoothHelper.BluetoothCallback {
 
     Resources res;
     SharedPreferences sharedLightPref;
+
+    SwitchPreference enablePreference;
+    ListPreference modePreference;
+    SeekBarPreference intensityPreference, thresholdPreference;
 
     BluetoothHelper btHelper;
 
@@ -32,20 +36,26 @@ public class LightSettingsActivity extends AppCompatPreferenceActivity implement
 
         setContentView(R.layout.settings_light);
         setupActionBar();
-        setTitle(R.string.title_activity_light_settings);
         addPreferencesFromResource(R.xml.pref_light);
 
         res = getResources();
 
         sharedLightPref = getPreferenceScreen().getSharedPreferences();
 
-        ListPreference modePreference = (ListPreference) findPreference(res.getString(R.string.pref_key_light_mode));
+        enablePreference = (SwitchPreference) findPreference(res.getString(R.string.pref_key_light_enabled));
+        modePreference = (ListPreference) findPreference(res.getString(R.string.pref_key_light_mode));
         setFreqEnabled(modePreference.findIndexOfValue(sharedLightPref.getString(res.getString(R.string.pref_key_light_mode), "")));
         if (Variables.lowPowerMode) modePreference.setEnabled(false);
 
+        intensityPreference = (SeekBarPreference) this.findPreference(res.getString(R.string.pref_key_light_intensity));
+        thresholdPreference = (SeekBarPreference) this.findPreference(res.getString(R.string.pref_key_light_threshold));
         setSeekBars();
 
+        Variables.updateSettingsScreen = false;
+
         btHelper = BluetoothHelper.getInstance();
+        btHelper.setInLightScreenFlag(true);
+        btHelper.setCallback(this);
     }
 
     /**
@@ -54,9 +64,9 @@ public class LightSettingsActivity extends AppCompatPreferenceActivity implement
     private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        setTitle(R.string.title_activity_light_settings);
     }
 
     @Override
@@ -68,6 +78,7 @@ public class LightSettingsActivity extends AppCompatPreferenceActivity implement
     @Override
     protected void onPause() {
         super.onPause();
+        btHelper.setInLightScreenFlag(false);
         sharedLightPref.unregisterOnSharedPreferenceChangeListener(this);
     }
 
@@ -75,100 +86,185 @@ public class LightSettingsActivity extends AppCompatPreferenceActivity implement
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         Preference preference = findPreference(key);
 
-        if (Variables.lowPowerMode) {
-            ListPreference mode = (ListPreference) findPreference(res.getString(R.string.pref_key_light_mode));
-            mode.setEnabled(false);
-        }
+        if (Variables.lowPowerMode) modePreference.setEnabled(false);
 
-//        if (Variables.updateSettingsScreen) {
-//            if (key.matches(res.getString(R.string.pref_key_light_enabled))) {
-//                SwitchPreference enabled = (SwitchPreference) findPreference(key);
-//                boolean newState = sharedPreferences.getBoolean(key, false);
-//                enabled.setChecked(newState);
-//                if (!newState) {
-//                    Variables.updateSettingsScreen = false;
-//                }
-//            } else if (key.matches(res.getString(R.string.pref_key_light_mode))) {
-//                ListPreference mode = (ListPreference) findPreference(key);
-//                mode.setSummary(mode.findIndexOfValue(sharedPreferences.getString(key, "")));
-//                Variables.updateSettingsScreen = false;
-//            }
-//        } else {
-        String newStateMsg = "<W=1;";
-
-        if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list.
-            ListPreference listPreference = (ListPreference) preference;
-            int index = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
-
-            // Set the summary to reflect the new value.
-            listPreference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
-
-            if (key.matches(res.getString(R.string.pref_key_light_mode))) {
-                setFreqEnabled(index);
-
-                newStateMsg += "L=" + listPreference.getValue() + ";>";
-            } else if (key.matches(res.getString(R.string.pref_key_light_freq))) {
-                newStateMsg += "F=" + listPreference.getValue() + ";>";
-            }
-        } else if (preference instanceof SwitchPreference) {
-            SwitchPreference switchPreference = (SwitchPreference) preference;
-
+        if (Variables.updateSettingsScreen) {
             if (key.matches(res.getString(R.string.pref_key_light_enabled))) {
-                newStateMsg += "L=";
+                boolean newState = sharedPreferences.getBoolean(key, false);
+                enablePreference.setChecked(newState);
+            } else if (key.matches(res.getString(R.string.pref_key_light_intensity))) {
+                SeekBarPreference intensity = (SeekBarPreference) findPreference(key);
 
-                if (switchPreference.isChecked()) {
-                    ListPreference lastState = (ListPreference) findPreference(res.getString(R.string.pref_key_light_mode));
-                    newStateMsg += lastState.getValue() + ";>";
-                } else {
-                    newStateMsg += "0;>";
+                int newIntensity = sharedPreferences.getInt(key, 50);
+                intensity.setSummary(this.getString(R.string.pref_light_intensity_summary).replace("$1", "" + newIntensity));
+            } else if (key.matches(res.getString(R.string.pref_key_light_mode))) {
+                int index = modePreference.findIndexOfValue(sharedPreferences.getString(key, "1"));
+                modePreference.setValue(sharedPreferences.getString(key, "1"));
+                modePreference.setSummary(index >= 0 ? modePreference.getEntries()[index] : null);
+                setFreqEnabled(index);
+            }
+
+            Variables.updateSettingsCounter--;
+            Variables.updateSettingsScreen = (Variables.updateSettingsCounter != 0);
+        } else {
+            String newStateMsg = "<W=1;";
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
+
+                // Set the summary to reflect the new value.
+                listPreference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+
+                if (key.matches(res.getString(R.string.pref_key_light_mode))) {
+                    setFreqEnabled(index);
+
+                    newStateMsg += "L=" + listPreference.getValue() + ";>";
+                } else if (key.matches(res.getString(R.string.pref_key_light_freq))) {
+                    newStateMsg += "F=" + listPreference.getValue() + ";>";
                 }
-            }
-        } else if (preference instanceof SeekBarPreference) {
-            if (key.matches(res.getString(R.string.pref_key_light_intensity))) {
-                newStateMsg += "I=";
+            } else if (preference instanceof SwitchPreference) {
+                SwitchPreference switchPreference = (SwitchPreference) preference;
 
-                int radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_intensity), 50);
-                preference.setSummary(this.getString(R.string.pref_light_intensity_summary).replace("$1", "" + radius));
+                if (key.matches(res.getString(R.string.pref_key_light_enabled))) {
+                    newStateMsg += "L=" + (switchPreference.isChecked() ? modePreference.getValue() : "0") + ";>";
+                }
+            } else if (preference instanceof SeekBarPreference) {
+                int radius = 0;
+
+                if (key.matches(res.getString(R.string.pref_key_light_intensity))) {
+                    newStateMsg += "I=";
+
+                    radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_intensity), 50);
+                    preference.setSummary(this.getString(R.string.pref_light_intensity_summary).replace("$1", "" + radius));
+                } else if (key.matches(res.getString(R.string.pref_key_light_threshold))) {
+                    newStateMsg += "P=";
+
+                    radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_threshold), 10);
+                    preference.setSummary(this.getString(R.string.pref_light_threshold_summary).replace("$1", "" + radius));
+                }
+
                 if (radius == 100) radius = 99;
                 newStateMsg += String.format("%02d", radius) + ";>";
-            } else if (key.matches(res.getString(R.string.pref_key_light_threshold))) {
-                newStateMsg += "P=";
+            }
 
-                int radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_threshold), 50);
-                preference.setSummary(this.getString(R.string.pref_light_threshold_summary).replace("$1", "" + radius));
-                if (radius == 100) radius = 99;
-                newStateMsg += String.format("%02d", radius) + ";>";
+            if (btHelper.getConnectionStatus()) {
+                btHelper.sendMessage(newStateMsg);
             }
         }
-
-        if (btHelper.getConnectionStatus()) {
-            btHelper.sendMessage(newStateMsg);
-        }
-//        }
     }
 
 
     public void setFreqEnabled(int index) {
         Preference preference_freq = findPreference(res.getString(R.string.pref_key_light_freq));
-
-        if (index == 0) {
-            preference_freq.setEnabled(false);
-        } else {
-            preference_freq.setEnabled(true);
-        }
+        preference_freq.setEnabled(index != 0);
     }
 
     public void setSeekBars() {
-        SeekBarPreference intensityPreference = (SeekBarPreference) this.findPreference(res.getString(R.string.pref_key_light_intensity));
-        SeekBarPreference thresholdPreference = (SeekBarPreference) this.findPreference(res.getString(R.string.pref_key_light_threshold));
         // Set seekbar properties:
         int radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_intensity), 50);
         intensityPreference.setSummary(this.getString(R.string.pref_light_intensity_summary).replace("$1", "" + radius));
         intensityPreference.setMin(1);
-        radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_threshold), 50);
+        radius = sharedLightPref.getInt(res.getString(R.string.pref_key_light_threshold), 10);
         thresholdPreference.setSummary(this.getString(R.string.pref_light_threshold_summary).replace("$1", "" + radius));
         thresholdPreference.setMin(10);
+    }
+
+    @Override
+    public void onMessageReceived(String message) {
+        if (message.startsWith("<U")) {
+            final String finalMessage = message;
+            final int value = Integer.valueOf(message.split(";")[1].substring(2));
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (finalMessage.contains("L=")) {
+                        updateLightState(value);
+                    } else if (finalMessage.contains("P=")) {
+                        setLowBattMode(value);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDeviceConnected() {
+    }
+
+    @Override
+    public void onDeviceDisconnected() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LightSettingsActivity.this.finish();
+            }
+        });
+    }
+
+    private void updateLightState(int newState) {
+        //TODO check if need this starting as true or can be direct with the checking in the end
+        Variables.updateSettingsScreen = true;
+
+        if (newState == 0) {
+            if (enablePreference.isChecked()) {
+                Variables.updateSettingsCounter++;
+                enablePreference.setChecked(false);
+            }
+        } else {
+            if (!enablePreference.isChecked()) {
+                Variables.updateSettingsCounter++;
+                enablePreference.setChecked(true);
+            }
+
+            if (!modePreference.getValue().equals(String.valueOf(newState))) {
+                Variables.updateSettingsCounter++;
+                modePreference.setValue(String.valueOf(newState));
+//            int index = modePreference.findIndexOfValue("1");
+//            modePreference.setSummary(index >= 0 ? modePreference.getEntries()[index] : null);
+//            setFreqEnabled(index);
+            }
+        }
+
+        Variables.updateSettingsScreen = (Variables.updateSettingsCounter != 0);
+    }
+
+    private void setLowBattMode(int lowBattMode) {
+        //TODO check if need this starting as true or can be direct with the checking in the end
+        Variables.updateSettingsScreen = true;
+        Variables.lowPowerMode = (lowBattMode != 0);
+
+//        sharedLightPref.unregisterOnSharedPreferenceChangeListener(this);
+
+        modePreference.setEnabled(!Variables.lowPowerMode);
+
+        if (Variables.lowPowerMode) {
+            if (!enablePreference.isChecked()) {
+                Variables.updateSettingsCounter++;
+                enablePreference.setChecked(true);
+            }
+
+            if (intensityPreference.getProgress() != 50) {
+                Variables.updateSettingsCounter++;
+                intensityPreference.setProgress(50);
+//                intensityPreference.setSummary(this.getString(R.string.pref_light_intensity_summary).replace("$1", "" + 50));
+            }
+
+        }
+
+        if (!modePreference.getValue().equals("1")) {
+            Variables.updateSettingsCounter++;
+            modePreference.setValue("1");
+//            int index = modePreference.findIndexOfValue("1");
+//            modePreference.setSummary(index >= 0 ? modePreference.getEntries()[index] : null);
+//            setFreqEnabled(index);
+        }
+
+//        sharedLightPref.registerOnSharedPreferenceChangeListener(this);
+        //TODO Check if works as expected with commented code
+
+        Variables.updateSettingsScreen = (Variables.updateSettingsCounter != 0);
     }
 }
